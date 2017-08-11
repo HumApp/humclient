d99d25913da5cd4cd87c7186387eaa9fd517b986
Brian Bohme <bohmebrian@gmail.com>
2017-08-11 16:39:08 -0400

    feat: profile page, issue: missing icons and formatting

--- js/components/profile/index.js
+++ js/components/profile/index.js
@@ -1,14 +1,112 @@
 import React, { Component } from 'react';
-import { Container, Content, Button, Icon, Text } from 'native-base';
+import {
+  Container,
+  Content,
+  Button,
+  Left,
+  Body,
+  Icon,
+  Text,
+  Right,
+  List,
+  ListItem,
+  Card,
+  CardItem,
+  SwipeRow,
+  View
+} from 'native-base';
+
 export default class Profile extends Component {
+
   render() {
     return (
       <Container>
-        <Content />
-        <Button vertical active>
-          <Icon name="md-person" />
-          <Text>Profile</Text>
-        </Button>
+        <Content>
+          <Card>
+            <CardItem header>
+              <Icon active name="ios-person" />
+              <Text>Personal Information</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Name</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Username</Text>
+              </Body>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem header>
+              <Icon active name="ios-musical-notes" />
+              <Text>Integrations</Text>
+            </CardItem>
+            <SwipeRow
+              rightOpenValue={-75}
+              body={
+                <CardItem>
+                  <Left>
+                    <Icon name="logo-apple" />
+                  </Left>
+                  <Body>
+                    <Text>Apple Music</Text>
+                  </Body>
+                  <Right>
+                    <Icon name="ios-checkmark-circle" />
+                  </Right>
+                </CardItem>
+              }
+              right={
+                <Button danger onPress={() => alert('Trash')}>
+                  <Icon active name="ios-close-circle-outline" />
+                </Button>
+              }
+            />
+            <CardItem>
+              <Left>
+                <Icon name="logo-apple" />
+              </Left>
+              <Body>
+                <Text>Spotify</Text>
+              </Body>
+              <Right>
+                <Icon name="ios-add" />
+              </Right>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem header>
+              <Icon active name="ios-settings" />
+              <Text>Settings</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Update Password</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Report an Issue</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>Delete Account</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
       </Container>
     );
   }
