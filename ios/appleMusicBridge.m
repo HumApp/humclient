163553a3a3d544edef34812098170c988665134f
Olivia Oddo <olivia.oddo@gmail.com>
2017-08-14 18:23:17 -0400

    added swift files

---
+++ ios/appleMusicBridge.m
@@ -0,0 +1,26 @@
+//
+//  reactBridge.m
+//  applemusic
+//
+//  Created by Olivia Oddo on 8/12/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(AuthorizationManager, NSObject)
+
+RCT_EXTERN_METHOD(requestCloudServiceAuthorization: (RCTResponseSenderBlock)callback);
+RCT_EXTERN_METHOD(requestMediaLibraryAuthorization: (RCTResponseSenderBlock)callback);
+
+@end
+
+
+@interface RCT_EXTERN_MODULE(MediaLibraryManager, NSObject)
+
+RCT_EXTERN_METHOD(createPlaylistIfNeeded: (RCTResponseSenderBlock)callback);
+
+@end
+
+
