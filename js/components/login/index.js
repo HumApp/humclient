96e542046aa506f177268903ac21bded2e6e1a72
deepseafishing <onejunekang@gmail.com>
2017-08-14 14:16:27 -0400

    merge: hum to humclient

--- js/components/login/index.js
+++ js/components/login/index.js
@@ -8,15 +8,40 @@ import {
   Form,
   Item,
   Input,
-  Label,
-  CardItem,
   Card,
+  CardItem,
+  Label,
   Icon
 } from 'native-base';
-import styles from './style'
+import styles from './style';
 import { Field, reduxForm } from 'redux-form';
+import firebase from 'firebase';
 
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: true,
+      email: '',
+      password: ''
+    };
+    this.login = this.login.bind(this);
+  }
+  async login() {
+    try {
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password);
+      console.log('Logged In');
+    } catch (error) {
+      console.log(error.toString());
+    }
+    this.setState({
+      email: '',
+      password: '',
+      loaded: true
+    });
+  }
   render() {
     return (
       <Container>
@@ -24,21 +49,28 @@ export default class SignUp extends Component {
           <Content>
             <Form style={styles.form}>
               <Text style={styles.header}>Login</Text>
+
               <Item floatingLabel>
                 <Label>Username</Label>
-                <Input autoCapitalize="none" />
+                <Input
+                  autoCapitalize="none"
+                  value={this.state.email}
+                  onChangeText={text => this.setState({ email: text })}
+                />
               </Item>
-              <Item floatingLabel>
+              <Item floatingLabel last>
                 <Label>Password</Label>
-                <Input autoCapitalize="none" secureTextEntry={true} />
+                <Input
+                  autoCapitalize="none"
+                  value={this.state.password}
+                  secureTextEntry={true}
+                  onChangeText={text => this.setState({ password: text })}
+                />
               </Item>
               <CardItem>
-                <Button rounded iconRight style={styles.login}
-                  onPress={() =>
-                    this.props.navigation.navigate('SignedIn', { signedIn: false })}
-                >
+                <Button iconRight style={styles.login} onPress={this.login}>
                   <Text style={{ fontSize: 18 }}>Login</Text>
-                  <Icon name='ios-arrow-forward' style={{ color: '#fff' }} />
+                  <Icon name="ios-arrow-forward" style={{ color: '#fff' }} />
                 </Button>
               </CardItem>
             </Form>
