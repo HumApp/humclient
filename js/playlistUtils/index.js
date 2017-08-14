a139d16206868202c5371c5965099e51d8d6eb63
deepseafishing <onejunekang@gmail.com>
2017-08-14 19:22:43 -0400

    add: save playlist to database

--- js/playlistUtils/index.js
+++ js/playlistUtils/index.js
@@ -1,60 +1,45 @@
-// import firebase from 'firebase';
+import firebase from 'firebase';
 
-// function savePlaylist(playlists) {
-//   Object.keys(playlists).forEach(key => {
-//     let newPlaylistId = firebase.database().ref(`playlists/`).push().key;
-//     firebase.database().ref(`playlists/${newPlaylistId}`).set({});
-//   });
-//   return;
-// }
+function savePlaylist(playlists) {
+  firebase.database().ref(`/songs`).orderByKey().once('value', songlist => {
+    songlist.forEach(song => console.log(song.child('appleId').val()));
+    playlists.forEach(playlist => {
+      let newPlaylist = {};
+      playlist.songs.forEach(song => {
+        newPlaylist[findOrCreateSong(song)] = true;
+      });
+      let newPlaylistId = firebase.database().ref(`playlists/`).push().key;
+      firebase.database().ref(`playlists/${newPlaylistId}`).set({
+        title: playlist.title,
+        creator: firebase.auth().currentUser.uid,
+        songs: newPlaylist
+      });
+    });
+  });
 
-// let obj = {
-//   playlistOne: {
-//     songs: [
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       },
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       },
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       },
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       }
-//     ]
-//   },
-//   playlistTwo: {
-//     songs: [
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       },
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       },
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       },
-//       {
-//         songName: 'NAME',
-//         artist: 'ARTIST',
-//         appleId: '21324324'
-//       }
-//     ]
-//   }
-// };
+  return;
+}
+
+function findOrCreateSong(song, songlist, provider) {
+  songlist.forEach(firesong => {
+    if (
+      firesong.child('title').val() === song.title &&
+      firesong.child('artist').val() === song.artist
+    ) {
+      if (!firesong.hasChild(provider)) {
+        firesong.database().update({
+          [provider]: song.id
+        });
+        return firesong.key;
+      } else {
+        const newSongId = firebase.database().ref('/songs').push().key;
+        firebase.database().ref(`/songs/${newSongId}`).set({
+          [provider]: song.id,
+          title: song.title,
+          artist: song.artist
+        });
+        return newSongId;
+      }
+    }
+  });
+}
