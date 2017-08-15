3ec46fc0df9d5076bf6176387c8d7a0cbec51327
Max Brodheim <max.brodheim@gmail.com>
2017-08-15 19:58:27 -0400

    adds spotify to cloud function

--- functions/index.js
+++ functions/index.js
@@ -20,26 +20,28 @@ exports.getSongId = functions.https.onRequest((req, response) => {
           })
           .catch(console.error);
         } else {
-          axios.get(makeSpotifySongQuery(reqSong.title, reqSong.artist), {
-            headers: {
-              // Accept: 'application/json',
-              Authorization: 'Bearer BQDr7U_bQgpM2l3R17synWX2SbtCvX2L3POtseIPGZv9lByJU9S404zs4Uf3KHyaNiYYL-uRu0WT5LOwlGtOam6EW4R3GE3m7aZHMXQPEJ770oQYfDVKIbeMPCKXV3FFE0XFA66SFaGmYq04-EdHyu6mDcr1dOgAo6t6Z4N6sCpiRQM46vsL5sn3r76mJyJlcPi4TxicKKF3lVUl0oFVUPJ6T9PMWkGkr2KOt0yvFZlA8Cpo7dtlX1qMlDPH5cT8VN9I8AM45u9qmcRGI2I0bW6e4roH5mV4o25tB53GRGMGD23NBMuot2fFfksyZjH8ZzUX',
-              // 'Content-Type': 'application/x-www-form-urlencoded'
-            }
+          axios.get(makeSpotifySongQuery(reqSong.title.replace(' ', '+'), reqSong.artist.replace(' ', '+')),
+          // axios.get('https://api.spotify.com/v1/search?q=track:The+Long+And+Winding+Road+artist:The+Beatles&type=track&market=us',
+          {
+           headers: {
+             "Authorization": "Bearer BQD57jmCLBcyccmoVzShwSV9gZLyBiljcr8OHO8zle03wLW74J76tEFiqLq3nwq_4ZuAVAihSAOJsXMUUZFjiu0-3S7-CI87bVuGts22eVaPB0LZ8S8lYaSnM6W-wgRQn2ueFTrXT3_B9NPhB30ZU52vWcgqAuAJWgo6xsxlx0nWi2QsqXbCkePbof8TkhmlqPxVNBwiKXnI_Utbr5_ewIC38V51_U9QGtknCL8NHBE-zwQ3bl6iVxRFEi244sRmBbHkJ8Movj2gzVXcpRLJpLSJRRC61XKN-TVxrxO80GhvZlqgdUO1YVrPCy6qIVHhDyXj",
+            //  "Content-Type": 'application/x-www-form-urlencoded'
+            //  Accept: 'application/json'
+           }
           })
-          .catch(console.error)
           .then(res => res.data)
           .then(json => {
             console.log('json.name', json.tracks.items[0].name);
-          });
+            response.send(json);
+          })
+          .catch(console.error);
         }
     })
     .catch(console.error);
 });
-
 function makeiTunesSongQuery(songTitle, songArtist) {
   return `https://itunes.apple.com/search?term=${songTitle} ${songArtist}&media=music`;
 }
 function makeSpotifySongQuery(songTitle, songArtist) {
-  return `https://api.spotify.com/v1/search?q=track:${songTitle} artist:${songArtist}&type=track&market=us`;
+  return `https://api.spotify.com/v1/search?q=track:${songTitle}+artist:${songArtist}&type=track&market=us`;
 }
