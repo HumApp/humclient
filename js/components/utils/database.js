a4aef6734c6833c9890dc65b1ec4076a400ec12e
Olivia Oddo <olivia.oddo@gmail.com>
2017-08-16 12:40:53 -0400

    fix

--- js/components/utils/database.js
+++ js/components/utils/database.js
@@ -1,22 +1,25 @@
 import * as firebase from 'firebase';
 
 export default class Database {
-    static saveMultiPlaylists(playlists, providerId) {
-      playlists.forEach(playlist => {
-        let newSong = {}
-        playlist.songs.forEach((song, index) => {
-          this.findOrCreateSong(song, providerId);
-            newSong[index] = {};
-            newSong[index].artist = song.artist;
-            newSong[index].title = song.title;
-        })
+  static saveMultiPlaylists(playlists, providerId) {
+    for (playlistName in playlists) {
+      if (playlists.hasOwnProperty(playlistName)) {
+        const playlist = playlists[playlistName];
+        let newSong = {};
+        playlist.forEach((fetchSong, idx) => {
+          this.findOrCreateSong(fetchSong, providerId);
+          newSong[idx] = {};
+          newSong[idx].artist = fetchSong.artist;
+          newSong[idx].title = fetchSong.title;
+        });
         const newPlaylistId = firebase.database().ref('playlists').push().key;
-          firebase.database().ref(`playlists/${newPlaylistId}`).set({
-          title: playlist.name,
-          creator: "Olivia",
+        firebase.database().ref(`playlists/${newPlaylistId}`).set({
+          title: playlistName,
+          creator: firebase.auth().currentUser.uid,
           songs: newSong
         });
-      })
+      }
+    }
   }
   static async findOrCreateSong(fetchSong, providerId) {
     try {
