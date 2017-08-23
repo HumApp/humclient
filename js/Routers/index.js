4c07b6e411d6b0e9c3f2f642f24993d3311fcafc
deepseafishing <onejunekang@gmail.com>
2017-08-23 16:07:32 -0400

    fix: login

--- js/Routers/index.js
+++ js/Routers/index.js
@@ -4,33 +4,25 @@ import SignedIn from './signedin';
 import SignedOut from './signedout';
 
 export const createRootNavigator = signedIn => {
-  return signedIn
-    ? StackNavigator(
-        {
-          SignedIn: {
-            screen: SignedIn,
-            navigationOptions: {
-              gesturesEnabled: false
-            }
-          }
-        },
-        {
-          headerMode: 'none',
-          mode: 'modal'
+  return StackNavigator(
+    {
+      SignedIn: {
+        screen: SignedIn,
+        navigationOptions: {
+          gesturesEnabled: false
         }
-      )
-    : StackNavigator(
-        {
-          SignedOut: {
-            screen: SignedOut,
-            navigationOptions: {
-              gesturesEnabled: false
-            }
-          }
-        },
-        {
-          headerMode: 'none',
-          mode: 'modal'
+      },
+      SignedOut: {
+        screen: SignedOut,
+        navigationOptions: {
+          gesturesEnabled: false
         }
-      );
+      }
+    },
+    {
+      headerMode: 'none',
+      mode: 'modal',
+      initialRouteName: signedIn ? 'SignedIn' : 'SignedOut'
+    }
+  );
 };
